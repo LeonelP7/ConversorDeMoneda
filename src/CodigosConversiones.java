d5560172c968020471f6868d6770498162989c97
leonelp <pereiraleonel287@gmail.com>
2024-06-29 18:40:27 -0500

    añadiendo la clase CodigosConversiones donde se guardan los codigos de moneda que soporta la aplicacion

---
+++ src/CodigosConversiones.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class CodigosConversiones {
+
+    Map<String, String> currencyCodes = new HashMap();
+
+    public CodigosConversiones() {
+        currencyCodes.put("USD", "Dolar estadounidense ");
+        currencyCodes.put("COP", "Peso Colombiano");
+        currencyCodes.put("ARS", "Peso Argentino");
+        currencyCodes.put("BOB", "Boliviano boliviano");
+        currencyCodes.put("BRL", "Real brasileño");
+        currencyCodes.put("CLP", "Peso chileno");
+
+    }
+
+    public Map<String, String> getCurrencyCodes() {
+        return currencyCodes;
+    }
+}
